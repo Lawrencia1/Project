6fcce64be3cf15d04f275523360b08435e8f5ac9
Lawrencia <ukatuifunanya@gmail.com>
2025-04-16 13:13:30 +0100

    project 3 commit

---
+++ Project_3_stepwise_solutions.R
@@ -0,0 +1,10 @@
+Steps for Project 3 solutions
+Step 1- Load in necessary files and install packages
+Step 2- Read in data into named files
+Step 3- Remove unwanted rows across all columns
+Step 4- Drop na values in data set
+Step 5- Select specific columns as needed 
+Step 6- Rename columns appropraitely
+Step 7- Arrange column values in ascending order
+Step 8- Remove automatic alphabetic ordering of x-axis
+Step 9- Plot bar chart
