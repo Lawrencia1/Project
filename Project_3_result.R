80b50802d1a4316d2fc5f186a3072a7794203f5f
Lawrencia <ukatuifunanya@gmail.com>
2025-04-17 12:55:19 +0100

    Second commint of remaining R-files

---
+++ Project_3_result.R
@@ -0,0 +1,6 @@
+Recommendation for project 3
+
+Based on the various financial performances of the different house types across various UK regions over the years
+It is recommended that property ivestment should be channelled towards detached house in London.
+This is due to the fact that despite market fluctuations across all years, detached houses demonstrate the highest appreciation over time. 
+Also, areas like London continue to command the highest average prices of UK houses, as such offerring strong potential for capital appreciation.  
---
+++ Question_project_1.R
@@ -0,0 +1,15 @@
+Questions for project 1
+
+Movie lens is a US based company in the internet and entrertainment domain that has an online database of information related to fils, television series, online streaming content- including cast, production crew, trivia, ratings and fan and critical views. You have been hired an an analyst for the company. You have been provided with the three data sets, asked to carry put detailed bussiness intelligence and come up with meaningful insights, to help the company address users in a better way. 
+
+a. How many movies belong to a particular genre
+
+b. Which movies have more than one genre
+
+c.Which genres are most liked by users
+
+d. Which movies have been most preferred by users
+
+e. Which movies are rated most of the time
+
+f. Any relationship between dermographic details of users and ratings of movies 
